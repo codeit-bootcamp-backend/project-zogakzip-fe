f444a29b5a92d28f2088e3bf96cccbbb6be3d6d3
drizzle96 <dlxkffldk37@gmail.com>
2024-03-16 21:58:21 +0900

    feat: 임시 에러 페이지

---
+++ src/app/error.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import { useEffect } from 'react'
+
+type ErrorProps = {
+  error: Error & { digest?: string }
+  reset: () => void
+}
+
+const Error = ({ error, reset }: ErrorProps) => {
+  // TODO: 디자인 요청 및 반영
+  useEffect(() => {
+    console.error(error)
+  }, [error])
+
+  return (
+    <div>
+      <h2>Something went wrong!</h2>
+      <button
+        onClick={
+          () => reset()
+        }
+      >
+        Try again
+      </button>
+    </div>
+  )
+}
+
+export default Error
