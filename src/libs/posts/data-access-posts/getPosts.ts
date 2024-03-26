5aa01a50791f8d643cd53c5990cf1364381d8faa
drizzle96 <dlxkffldk37@gmail.com>
2024-03-26 23:59:33 +0900

    feat: getPost에 keyword 쿼리파람 반영

--- src/libs/posts/data-access-posts/getPosts.ts
+++ src/libs/posts/data-access-posts/getPosts.ts
@@ -5,8 +5,8 @@ import { getRequest } from '@services/api/requests'
 import { PostsSearchParams, Post, PaginationResponse } from '@services/api/types'
 
 const getPosts = async (groupId: number, { sortBy, keyword, isPublic, page = 1 }: PostsSearchParams) => {
-  // TODO-1: sortBy, keyword 백엔드 개발 완료 시 추가
-  const response = await getRequest<PaginationResponse<Post>>(`/groups/${groupId}/posts?isPublic=${isPublic}&page=${page}&pageSize=${isPublic ? PUBLIC_POSTS_PAGE_SIZE : PRIVATE_POSTS_PAGE_SIZE}`, { next: { tags: ['posts'] } })
+  // TODO-1: sortBy 백엔드 개발 완료 시 추가
+  const response = await getRequest<PaginationResponse<Post>>(`/groups/${groupId}/posts?keyword=${keyword}&isPublic=${isPublic}&page=${page}&pageSize=${isPublic ? PUBLIC_POSTS_PAGE_SIZE : PRIVATE_POSTS_PAGE_SIZE}`, { next: { tags: ['posts'] } })
   return response
 }
 
