22d1b500c0879a5669dffcf00bc8ef6689ab340a
drizzle96 <dlxkffldk37@gmail.com>
2024-03-27 10:11:34 +0900

    feat: sortBy 쿼리파람 반영

--- src/libs/groups/data-access-groups/getGroups.ts
+++ src/libs/groups/data-access-groups/getGroups.ts
@@ -3,8 +3,7 @@ import { getRequest } from '@services/api/requests'
 import { GroupsSearchParams, Group, PaginationResponse } from '@services/api/types'
 
 const getGroups = async ({ sortBy, keyword, isPublic, page = 1 }: GroupsSearchParams) => {
-  // TODO-1: sortBy 백엔드 개발 완료 시 추가
-  const response = await getRequest<PaginationResponse<Group>>(`/groups?&keyword=${keyword}&isPublic=${isPublic}&page=${page}&pageSize=${isPublic ? PUBLIC_GROUPS_PAGE_SIZE : PRIVATE_GROUPS_PAGE_SIZE}`, { next: { tags: ['groups'] } })
+  const response = await getRequest<PaginationResponse<Group>>(`/groups?&keyword=${keyword}&sortBy=${sortBy}&isPublic=${isPublic}&page=${page}&pageSize=${isPublic ? PUBLIC_GROUPS_PAGE_SIZE : PRIVATE_GROUPS_PAGE_SIZE}`, { next: { tags: ['groups'] } })
   return response
 }
 
--- src/libs/posts/data-access-posts/getPosts.ts
+++ src/libs/posts/data-access-posts/getPosts.ts
@@ -5,8 +5,7 @@ import { getRequest } from '@services/api/requests'
 import { PostsSearchParams, Post, PaginationResponse } from '@services/api/types'
 
 const getPosts = async (groupId: number, { sortBy, keyword, isPublic, page = 1 }: PostsSearchParams) => {
-  // TODO-1: sortBy 백엔드 개발 완료 시 추가
-  const response = await getRequest<PaginationResponse<Post>>(`/groups/${groupId}/posts?keyword=${keyword}&isPublic=${isPublic}&page=${page}&pageSize=${isPublic ? PUBLIC_POSTS_PAGE_SIZE : PRIVATE_POSTS_PAGE_SIZE}`, { next: { tags: ['posts'] } })
+  const response = await getRequest<PaginationResponse<Post>>(`/groups/${groupId}/posts?keyword=${keyword}&sortBy=${sortBy}&isPublic=${isPublic}&page=${page}&pageSize=${isPublic ? PUBLIC_POSTS_PAGE_SIZE : PRIVATE_POSTS_PAGE_SIZE}`, { next: { tags: ['posts'] } })
   return response
 }
 
