2215745b6f8ad4a5b63bbd4f85737507f1aff903
drizzle96 <dlxkffldk37@gmail.com>
2024-03-29 18:39:37 +0900

    fix: keyword에 encodeURIComponent 처리

--- src/libs/groups/data-access-groups/getGroups.ts
+++ src/libs/groups/data-access-groups/getGroups.ts
@@ -3,7 +3,7 @@ import { getRequest } from '@services/api/requests'
 import { GroupsSearchParams, Group, PaginationResponse } from '@services/api/types'
 
 const getGroups = async ({ sortBy, keyword, isPublic, page = 1 }: GroupsSearchParams) => {
-  const response = await getRequest<PaginationResponse<Group>>(`/groups?&keyword=${keyword}&sortBy=${sortBy}&isPublic=${isPublic}&page=${page}&pageSize=${isPublic ? PUBLIC_GROUPS_PAGE_SIZE : PRIVATE_GROUPS_PAGE_SIZE}`, { next: { tags: ['groups'] } })
+  const response = await getRequest<PaginationResponse<Group>>(`/groups?&keyword=${encodeURIComponent(keyword)}&sortBy=${sortBy}&isPublic=${isPublic}&page=${page}&pageSize=${isPublic ? PUBLIC_GROUPS_PAGE_SIZE : PRIVATE_GROUPS_PAGE_SIZE}`, { next: { tags: ['groups'] } })
   return response
 }
 
--- src/libs/posts/data-access-posts/getPosts.ts
+++ src/libs/posts/data-access-posts/getPosts.ts
@@ -5,7 +5,7 @@ import { getRequest } from '@services/api/requests'
 import { PostsSearchParams, Post, PaginationResponse } from '@services/api/types'
 
 const getPosts = async (groupId: number, { sortBy, keyword, isPublic, page = 1 }: PostsSearchParams) => {
-  const response = await getRequest<PaginationResponse<Post>>(`/groups/${groupId}/posts?keyword=${keyword}&sortBy=${sortBy}&isPublic=${isPublic}&page=${page}&pageSize=${isPublic ? PUBLIC_POSTS_PAGE_SIZE : PRIVATE_POSTS_PAGE_SIZE}`, { next: { tags: ['posts'] } })
+  const response = await getRequest<PaginationResponse<Post>>(`/groups/${groupId}/posts?keyword=${encodeURIComponent(keyword)}&sortBy=${sortBy}&isPublic=${isPublic}&page=${page}&pageSize=${isPublic ? PUBLIC_POSTS_PAGE_SIZE : PRIVATE_POSTS_PAGE_SIZE}`, { next: { tags: ['posts'] } })
   return response
 }
 
