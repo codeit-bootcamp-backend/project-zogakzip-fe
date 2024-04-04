e86e9bef44fdd4c983d4354f2c5c3737c7e3f15b
drizzle96 <dlxkffldk37@gmail.com>
2024-04-04 13:56:23 +0900

    fix: 컨펌 모달 배경 스크롤 방지

--- src/libs/shared/modal/useConfirmModal.tsx
+++ src/libs/shared/modal/useConfirmModal.tsx
@@ -37,8 +37,19 @@ const useConfirmModal = () => {
 
   useEffect(() => {
     if (!modalRef.current) return
-    if (isOpened) modalRef.current.showModal()
-    else modalRef.current.close()
+
+    if (isOpened) {
+      modalRef.current.showModal()
+      document.body.style.overflow = 'hidden'
+    }
+    else {
+      modalRef.current.close()
+      document.body.style.overflow = 'auto'
+    }
+
+    return () => {
+      document.body.style.overflow = 'auto'
+    }
   }, [isOpened])
 
   return { renderConfirmModal, openConfirmModal }
